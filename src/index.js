e1c28676963136340d211b3f1964b5722554a355
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-24 15:54:54 +0300

    minimal style
    зробила основний функціонал без використання бібліотек і без синтаксису async/await

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = "37602273-ee76d535e0302dd8a6c170e2a"
 let page = 1
-let userFetch = null
+let userRequest = null
 const formEl = document.querySelector('.search-form')
 const galleryEl = document.querySelector('.gallery')
 const loadMore = document.querySelector('.load-more')
-const inputEl = document.querySelector('input')
-console.log(inputEl);
+
 
 loadMore.addEventListener('click', onNextPage) 
 
 function onNextPage() {
+  
   page += 1
-  fetchPhoto(userFetch)
+  fetchPhoto(userRequest)
     .then((data) => {
       (galleryEl.insertAdjacentHTML("beforeend", creatMarkupCard(data.hits)))
       console.log(data.hits);
@@ -24,7 +24,7 @@ function onNextPage() {
   
 }
 function fetchPhoto(userFetch) {
-  return fetch(`${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${userFetch}&page=${page}&per_page=4`)
+  return fetch(`${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${userFetch}&page=${page}&per_page=40`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.statusText)
@@ -35,7 +35,7 @@ function fetchPhoto(userFetch) {
 
 function creatMarkupCard(array) {
   return array.map(({ webformatURL, largeImageURL,tags,likes,views,comments,downloads }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" class="photo" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes ${likes}</b>
@@ -58,14 +58,11 @@ function creatMarkupCard(array) {
 formEl.addEventListener('submit', sendRequest)
 function sendRequest(e) {
     e.preventDefault()
-    
-    const userRequest = e.target.firstElementChild.value.trim()
-
+    userRequest = e.target.firstElementChild.value.trim()
     e.target.firstElementChild.value = ""
     if (userRequest === "") {
         return
     }
-
     console.log(userRequest);
   fetchPhoto(userRequest)
     .then((data) => {
